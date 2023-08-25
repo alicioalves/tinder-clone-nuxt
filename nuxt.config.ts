322e398d9106ccbe9e50ba20910e6f56276843e2
Alicio Alves <alicio.alvesm@gmail.com>
2023-08-24 21:43:03 -0300

    chore: add TailwindCSS

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	// Modules
+	modules: ['@nuxtjs/tailwindcss'],
+
+	// DevTools
 	devtools: { enabled: true }
 })
