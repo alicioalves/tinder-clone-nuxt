20eccae7ebfe810dacc7de9ffb3701b251103621
Alicio Alves <alicio.alvesm@gmail.com>
2023-08-24 21:55:22 -0300

    chore: add VueUse

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	// Modules
-	modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', '@nuxt/image', 'nuxt-icon'],
+	modules: [
+		'@nuxtjs/tailwindcss',
+		'@pinia/nuxt',
+		'@nuxt/image',
+		'nuxt-icon',
+		'@vueuse/nuxt'
+	],
 
 	// DevTools
 	devtools: { enabled: true }
