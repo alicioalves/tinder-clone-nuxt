3c44da73ed63f212746cbbb0db5c32d190ab735d
Alicio Alves <alicio.alvesm@gmail.com>
2023-08-24 21:45:35 -0300

    chore: add Pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	// Modules
-	modules: ['@nuxtjs/tailwindcss'],
+	modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt'],
 
 	// DevTools
 	devtools: { enabled: true }
