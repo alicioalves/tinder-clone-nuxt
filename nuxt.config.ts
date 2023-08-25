3644733cadeb47954b53603ab643c2a201e02e34
Alicio Alves <alicio.alvesm@gmail.com>
2023-08-24 21:53:58 -0300

    chore: add Nuxt Icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	// Modules
-	modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', '@nuxt/image'],
+	modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', '@nuxt/image', 'nuxt-icon'],
 
 	// DevTools
 	devtools: { enabled: true }
