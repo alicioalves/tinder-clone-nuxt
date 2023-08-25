02dcc71f21b05e28c06c7617983a133c53f20d4d
Alicio Alves <alicio.alvesm@gmail.com>
2023-08-24 21:47:51 -0300

    chore: add Nuxt Image

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	// Modules
-	modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt'],
+	modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', '@nuxt/image'],
 
 	// DevTools
 	devtools: { enabled: true }
