3241b45da23e1167e12debd0eb598aad3eebe767
Alicio Alves <alicio.alvesm@gmail.com>
2023-09-14 23:05:22 -0300

    feat: logo icons

---
+++ components/global/Flame.vue
@@ -0,0 +1,27 @@
+<template>
+	<svg
+		width="33"
+		height="40"
+		viewBox="0 0 33 40"
+		fill="none"
+		xmlns="http://www.w3.org/2000/svg"
+	>
+		<path
+			d="M32.623 22.752c0 8.998-7.294 16.291-16.291 16.291S.041 31.75.041 22.753c0-5.856 3.089-10.23 7.726-13.1 0 0-.168 5.877 2.183 6.045 2.351.168 8.901-5.542 7.39-15.45 12.764 6.55 15.283 17.24 15.283 22.504Z"
+			fill="url(#a)"
+		/>
+		<defs>
+			<linearGradient
+				id="a"
+				x1="29.096"
+				y1="8.309"
+				x2=".041"
+				y2="30.982"
+				gradientUnits="userSpaceOnUse"
+			>
+				<stop stop-color="#EE805F" />
+				<stop offset="1" stop-color="#EA4080" />
+			</linearGradient>
+		</defs>
+	</svg>
+</template>
